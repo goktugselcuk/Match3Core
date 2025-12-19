bfa41a4432cc8b522c249e6395b4b49110787df3
Göktuğ Selçuk <gselcuk1@binghamton.edu>
2025-12-19 16:26:59 +0300

    Added match detection, clearing, and gravity mechanics
    - Implemented horizontal and vertical match detection (3+ tiles)
    - Added safe match resolution using HashSet to avoid duplicates
    - Implemented tile clearing logic (logical grid + GameObject destruction)
    - Added gravity system to collapse tiles downward into empty slots
    - Board now resolves matches and applies gravity correctly (no refill yet)

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -20,11 +20,26 @@ public class Board : MonoBehaviour
 
     private Tile[,] tiles;
 
-    
+    [Header("ColorSetup")]
+
+    public Color[] tileColors;
+
+
     // Start is called before the first frame update
     void Start()
     {
         GenerateBoard();
+        var tileMatches=FindAllMatches();
+
+        foreach( Tile tile in tileMatches)
+        {
+            tile.SetDim(true);
+        }
+
+
+
+        ClearAllMatches(tileMatches);
+        ApplyGravity();
     }
 
     private void GenerateBoard()
@@ -42,12 +57,175 @@ public class Board : MonoBehaviour
 
                 Tile newTile = Instantiate(tilePrefab, spawnPos, Quaternion.identity,transform);
 
-                newTile.Init(x, y, 0);
+                int randomType = UnityEngine.Random.Range(0, tileColors.Length);
+                newTile.Init(x, y, randomType);
+                newTile.SetColor(tileColors[randomType]);
 
                 tiles[x, y] = newTile;
             }
         }
     }
+
+    public HashSet<Tile> FindAllMatches()
+    {
+       
+        HashSet<Tile> matches = new HashSet<Tile>();
+
+        // HORIZONTAL CHECK
+
+        for(int y = 0; y< height; y++)
+        {
+            int runStreak = 1;
+            for (int x=1; x < width; x++)
+            {
+                if (tiles[x, y].typeID == tiles[x-1, y].typeID)
+                {
+                    runStreak++;
+                }
+
+                else
+                {
+                    if (runStreak >= 3)
+                    {
+                        for(int k = 0; k < runStreak; k++)
+                        {
+                            matches.Add(tiles[x - 1 - k, y]);
+                        }
+                       
+                    }
+
+
+                    runStreak = 1;
+                }
+            }
+
+             // END OF THE ROW HANDLING
+            if (runStreak >= 3)
+            {
+                for (int k = 0; k < runStreak; k++)
+                {
+                    matches.Add(tiles[width - 1 - k, y]);
+                }
+            }
+
+        }
+
+        // VERTICAL CHECK
+        
+        for(int x = 0; x < width; x++)
+        {
+            int runStreak = 1;
+
+            for(int y = 1; y < height; y++)
+            {
+                if(tiles[x,y].typeID == tiles[x, y - 1].typeID)
+                {
+                    runStreak++;
+                }
+
+                else
+                {
+                    if (runStreak >= 3)
+                    {
+                        for(int k = 0; k < runStreak; k++)
+                        {
+                            matches.Add(tiles[x, y - 1 - k]);
+                        }
+                    }
+                    runStreak = 1;
+                }
+            }
+
+            // END OF COLUMN HANDLING
+
+
+            if(runStreak >= 3)
+                    {
+                for (int k = 0; k < runStreak; k++)
+                {
+                    matches.Add(tiles[x, height- 1 - k]);
+                }
+            }
+
+
+        }
+
+        return matches;
+    }
+
+    // Clear all tiles in matches from the array and from the scene
+    void ClearAllMatches(HashSet<Tile> matches)
+    {
+        foreach(Tile tile in matches)
+        {
+            int x = tile.x;
+            int y = tile.y;
+
+            tiles[x, y] = null;
+            Destroy(tile.gameObject);
+        }
+    }
+
+    void ApplyGravity()
+    {
+        for(int x=0; x< width; x++)
+        {
+            for(int y=0; y < height; y++)
+            {
+                if (tiles[x, y] == null)
+                {
+                    for(int aboveY=y; aboveY<height; aboveY++)
+                    {
+                        if (tiles[x, aboveY] != null)
+                        {
+                            tiles[x, y] = tiles[x, aboveY];
+                            tiles[x, aboveY] = null;
+                            tiles[x, y].y = y;
+
+                            tiles[x, y].transform.position = new Vector3(tiles[x, y].transform.position.x,
+                                                                                                tiles[x, y].transform.position.y - (aboveY - y) * tileSize, 0f);
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        float offsetX = -(width - 1) * tileSize * 0.5f;
+        float offsetY = -(height - 1) * tileSize * 0.5f;
+
+        return new Vector3(
+            x * tileSize + offsetX,
+            y * tileSize + offsetY,
+            0f
+        );
+    }
+
+    public void RefillBoard()
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y<height; y++)
+            {
+                if (tiles[x, y] == null)
+                {
+                    Tile newTile = Instantiate(tilePrefab, GetWorldPosition(x, y), Quaternion.identity, transform);
+                    int randomType = UnityEngine.Random.Range(0, tileColors.Length);
+
+                    newTile.Init(x, y, randomType);
+                    newTile.SetColor(tileColors[randomType]);
+
+                    tiles[x, y] = newTile;
+                }
+            }
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -9,6 +9,8 @@ public class Tile : MonoBehaviour
 
     public int typeID;
 
+    SpriteRenderer sr;
+
     public void Init(int x, int y, int typeID)
     {
         this.x = x;
@@ -17,15 +19,32 @@ public class Tile : MonoBehaviour
         name = $"Tile_{x}_{y}";
     }
 
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetColor(Color c)
+    {
+        sr.color = c;
+    }
+
+    public void SetDim(bool dim)
+    {
+        var c = GetComponent<SpriteRenderer>().color;
+        c.a = dim ? 0.35f : 1f;
+        GetComponent<SpriteRenderer>().color = c;
     }
 }
